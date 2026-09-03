fb6ebdb3ed592a2ea016136ea312acd892c03058
jiayiding <2906371967@qq.com>
2026-09-03 20:39:14 +1000

    feat: add manual care records and history updates

---
+++ PlantNest/Views/CareHistory/AddCareRecordView.swift
@@ -0,0 +1,18 @@
+//
+//  AddCareRecordView.swift
+//  PlantNest
+//
+//  Created by Djy on 03/09/2026.
+//
+
+import SwiftUI
+
+struct AddCareRecordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddCareRecordView()
+}
